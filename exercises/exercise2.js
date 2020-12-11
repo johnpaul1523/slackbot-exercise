4b94deef650694a36ab580161125ae90a45320c8
John Cox <johnpaul.cox01@gmail.com>
2020-12-10 19:23:03 -0500

    initial slackbot commit

--- exercises/exercise2.js
+++ exercises/exercise2.js
@@ -1,15 +1,21 @@
 // Set this assignment to true if you do want to use it.
-module.exports.ACTIVATE_BOT = false;
+module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
   // Write an interaction where one user says "I'm tired", the bot says "Wake up!"
   //
   // mychatbot> I'm tired
   // mychatbot responds "Wake up!"
-  
-
-
-
+  {
+    label: 'Wake Up Prompt',
+    prompt: 'I\'m tired',
+    handler: function () {
+      return 'Wake Up!';
+    },
+    isReply: false,
+    isCaseSensitive: false,
+    isListening: false,
+  },
 
   // Write an interaction where one user asks the bot 
   // "What's the area of a circle with radius X?" where X is any number
@@ -17,7 +23,18 @@ module.exports.botScripts = [
   // 
   // mychatbot> mychatbot What's the area of a circle with radius 5?
   // mychatbot responds to user "The area is 78.5398...."
-  
+  {
+    label: 'Circle Area Prompt',
+    prompt: 'What is the area of a circle with radius #{radiusCircle}?',
+    handler: function calculateArea (params) {
+      var circleArea = params.radiusCircle * params.radiusCircle * Math.PI;
+      return (
+        'The area is ' + 
+        circleArea
+      );
+    },
+    isListening: true,
+  },
 
 
 
@@ -28,4 +45,12 @@ module.exports.botScripts = [
   // 
   // mychatbot> mychatbot Tell me a story with Jack and Jill
   // mychatbot responds to user "Jack and Jill went up the hill to fetch a pail of water."
+  {
+    label: 'Story Prompt',
+    prompt: 'Tell me a story with #{NAME1} and #{NAME2}',
+    handler: function (params) {
+      return params.NAME1 + ' and ' + params.NAME2 + ' went up the hill to fetch a pail of water.';
+    },
+    isListening: true,
+  },
 ];
--- exercises/exercise3.js
+++ exercises/exercise3.js
@@ -1,12 +1,102 @@
 // Set this assignment to true if you do want to use it.
-module.exports.ACTIVATE_BOT = false;
+module.exports.ACTIVATE_BOT = true;
 
 module.exports.botScripts = [
   {
-    label: 'An Example Script',
-    prompt: 'Good Evening',
+    label: 'Greeting',
+    prompt: 'Hello',
     handler: function () {
-      return 'Good Evening to You!';
+      return 'Hello, welcome to JP\'s baseball info bot. Please enter your name to get started.';
     },
+    isListening: true,
+  },
+  {
+    label: 'Name Entry',
+    prompt: 'My name is #{firstName}',
+    handler: function (params) {
+      return 'Thank you ' + params.firstName + ' how can I help you today?';
+    },
+    isListening: true,
+  },
+  {
+    label: 'Batting Average Calculation',
+    prompt: 'If I have #{seasonHits} hits and #{seasonAtBats} at bats what is my batting average?',
+    handler: function (params) {
+      var battingAverage = params.seasonHits / params.seasonAtBats;
+      return 'Your batting average is ' + battingAverage;
+    },
+    isListening: true,
+  },
+  {
+    label: 'World Series Question',
+    prompt: 'Who will win the World Series in 2021?',
+    handler: function (params) {
+      var baseballTeams = ['New York Yankees','Boston Red Sox','Los Angeles Dodgers','Houston Astros'];
+      var winningTeam = baseballTeams[Math.floor(Math.random() * baseballTeams.length)];
+      return 'The ' + winningTeam + ' , who do you think will?';
+    },
+    isListening: true,
+  },
+  {
+    label: 'World Series User Answer',
+    prompt: 'I think it will be the #{baseballTeam}',
+    handler: function (params) {
+      if (params.baseballTeam === 'New York Yankees') {
+        return 'Possibly, but they always choke in the playoffs';
+      }
+      else if (params.baseballTeam === 'Boston Red Sox'){
+        return 'If they spend more money, sure';
+      }
+      else if (params.baseballTeam === 'Houston Astros'){
+        return 'If they can\'t cheat, they can\'t win!';
+      }
+      else if (params.baseballTeam === 'Los Angeles Dodgers'){
+        return 'Two years in a row? I doubt it.';
+      }
+      else {
+        return 'Ha, no shot pal';
+      }
+    },
+    isListening: true,
+  },
+  {
+    label: 'Highest Winning Percentage',
+    prompt: 'Which team in the American League had the highest winning percentage last year?',
+    handler: function () {
+      var winningPercentages = {
+        'Tampa Rays': 0.667,
+        'New York Yankees': 0.550,
+        'Toronto Blue Jays': 0.533,
+        'Baltimore Orioles': 0.417,
+        'The Boston Red Sox': 0.400
+      };
+      var highestPercentage = Math.max.apply(null, Object.values(winningPercentages)),
+        val = Object.keys(winningPercentages).find(function (a) {
+          return winningPercentages[a] === highestPercentage;
+        });
+
+      return 'The ' + val;
+    },
+    isListening: true,
+  },
+  {
+    label: 'Lowest Winning Percentage',
+    prompt: 'Which team in the American League had the lowest winning percentage last year?',
+    handler: function () {
+      var winningPercentages = {
+        'Tampa Rays': 0.667,
+        'New York Yankees': 0.550,
+        'Toronto Blue Jays': 0.533,
+        'Baltimore Orioles': 0.417,
+        'Boston Red Sox': 0.400
+      };
+      var lowestPercentage = Math.min.apply(null, Object.values(winningPercentages)),
+        val = Object.keys(winningPercentages).find(function (a) {
+          return winningPercentages[a] === lowestPercentage;
+        });
+
+      return 'The ' + val;
+    },
+    isListening: true,
   },
 ];
